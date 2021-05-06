4b59fa6d0afe2c33247e638dfb3b6e4e08fdce66
rcd1337 <56580332+rcd1337@users.noreply.github.com>
2021-05-06 05:56:15 -0300

    implemented client() and added messages

--- divulga_tudo/sistema/views.py
+++ divulga_tudo/sistema/views.py
@@ -1,4 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import render
+
+from .models import Client, Ad
 
 def index(request):
     return render(request, "sistema/index.html")
@@ -7,6 +10,30 @@ def ad(request):
     return render(request, "sistema/ad.html")
 
 def client(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+
+        if not name:
+            messages.error(request, f"Nome inválido.")
+            return render(request, "sistema/client.html")
+
+        # Checks if client's name is already registered
+        try:
+            Client.objects.get(name=name)
+            messages.error(request, f"Nome já cadastrado.")
+            return render(request, "sistema/client.html")
+        except Client.DoesNotExist:
+            pass
+        
+        # Attempts to register new client to database
+        try:
+            new_client = Client(name=name)
+            new_client.save()
+            messages.error(request, f"Cadastro efetuado com sucesso.")
+        except Exception:
+            messages.error(request, f"Cadastro não efetuado.")
+            return render(request, "sistema/client.html")
+
     return render(request, "sistema/client.html")
 
 def report(request):
